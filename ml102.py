402a629e4830fc4421dfa0734818ab37e78651f8
Jian Dai - Regular <daij12@rescomp5004.gene.com>
2017-02-09 07:27:55 -0800

    git rm files

--- ml102.py
+++ ml102.py
@@ -52,18 +52,14 @@ X = array[:,0:4]
 Y = array[:,4]
 #%%
 validation_size = 0.20
-#%%
+
+
 seed = 7
-quit()
-
-
-
 
 #%%
 X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
 
 #%%
-seed = 7
 scoring = 'accuracy'
 #%%
 models = []
--- tst/tst.py
+++
@@ -1,3 +0,0 @@
-from windows import call_subprocess_Popen
-x=call_subprocess_Popen(['g++', '-v'])
-print(x.pid)
