bbb4bd72e4d0f6aa662a5f7aee5380d8093adca0
Jian Dai - Regular <daij12@rescomp5001.gene.com>
2017-02-09 13:49:06 -0800

    use semeion data

--- ml101.py
+++ ml101.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 """
+Version <20170129 by Jian: ref to Jason Brownlee's blog
+http://machinelearningmastery.com/machine-learning-in-python-step-by-step/ 
 Version 20170129 by Jian: issue /w sklearn version
-ImportError: cannot import name model_selection
-result:
 >>> sklearn.__version__
 '0.15.2'
 >>> sklearn.__path__
 '/gne/research/apps/python/2.7.7/x86_64-linux-2.6-rhel6/lib/python2.7/site-packages/scikit_learn-0.15.2-py2.7-linux-x86_64.egg'
 
-Version 20170208 by Jian: install sklearn locally
+Version 20170208 by Jian: pip install sklearn locally
 pip install --user -U scikit-learn
 """
 import os
@@ -16,65 +16,61 @@ import sys
 sys.path.insert(0, os.path.expanduser('~')+'/.local/lib/python2.7/site-packages') # force to use local packages
 
 
-#%%
-import pandas
-#%%
+
 url='https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
 #%%
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+import pandas
 dataset = pandas.read_csv(url, names=names)
+
+#print(dir(dataset))
+print(dataset.describe())
 print(dataset.groupby('class').size())
-#%%
-#%%
-import matplotlib.pyplot as plt
-#%%
-dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
-plt.show()
-#%%
-dataset.hist()
-plt.show()
-#%%
-pandas.tools.plotting.scatter_matrix(dataset)
-plt.show()
 
 #%%
-#import sklearn
-from sklearn import model_selection
-from sklearn import linear_model
-from sklearn import discriminant_analysis
-from sklearn import neighbors
-from sklearn import tree
-from sklearn import naive_bayes
-from sklearn import svm
-from sklearn import metrics
+#import matplotlib.pyplot as plt
 #%%
+#dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+#plt.show()
+#%%
+#dataset.hist()
+#plt.show()
+#%%
+#pandas.tools.plotting.scatter_matrix(dataset)
+#plt.show()
+
+
 array = dataset.values
-#%%
 X = array[:,0:4]
 Y = array[:,4]
-#%%
+
 validation_size = 0.20
-#%%
 seed = 7
-
-
-
-
-#%%
+from sklearn import model_selection
 X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
 
-#%%
-seed = 7
+
+
+
+
 scoring = 'accuracy'
 #%%
 models = []
+from sklearn import linear_model
 models.append(('LR', linear_model.LogisticRegression()))
+from sklearn import discriminant_analysis
 models.append(('LDA', discriminant_analysis.LinearDiscriminantAnalysis()))
+from sklearn import neighbors
 models.append(('KNN', neighbors.KNeighborsClassifier()))
+from sklearn import tree
 models.append(('CART',tree.DecisionTreeClassifier()))
+from sklearn import naive_bayes
 models.append(('NB', naive_bayes.GaussianNB()))
+from sklearn import svm
 models.append(('SVM',svm.SVC()))
+
 # evaluate each model in turn
+seed = 11
 results = []
 names = []
 for name, model in models:
@@ -84,15 +80,18 @@ for name, model in models:
 	names.append(name)
 	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
 	print(msg)
- 
+
+
  
 #%%
-fig = plt.figure()
-fig.suptitle('Algorithm Comparison')
-ax = fig.add_subplot(111)
-plt.boxplot(results)
-ax.set_xticklabels(names)
-plt.show()
+ 
+#%%
+#fig = plt.figure()
+#fig.suptitle('Algorithm Comparison')
+#ax = fig.add_subplot(111)
+#plt.boxplot(results)
+#ax.set_xticklabels(names)
+#plt.show()
 
 #%%
 knn = neighbors.KNeighborsClassifier()
@@ -100,6 +99,11 @@ knn = neighbors.KNeighborsClassifier()
 knn.fit(X_train, Y_train)
 #%%
 predictions = knn.predict(X_validation)
+
+
+
+
+from sklearn import metrics
 #%%
 metrics.accuracy_score(Y_validation, predictions)
 #%%
--- ml102.py
+++ ml102.py
@@ -1,103 +1,101 @@
 # -*- coding: utf-8 -*-
 """
 Version 20170129 by Jian: issue /w sklearn version
-ImportError: cannot import name model_selection
-result:
 >>> sklearn.__version__
 '0.15.2'
 Version 20170208 by Jian: install sklearn locally
 pip install --user -U scikit-learn
+Version 20170209 by Jian: use semeion data as another example
 """
 import os
 import sys
 sys.path.insert(0, os.path.expanduser('~')+'/.local/lib/python2.7/site-packages')
 
+import pandas as pd
+dataset = pd.read_table('https://archive.ics.uci.edu/ml/machine-learning-databases/semeion/semeion.data',sep='\s+',header=None,engine='python')
 
-#%%
-import pandas
-#%%
-url='https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
-#%%
-names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-dataset = pandas.read_csv(url, names=names)
-print(dataset.groupby('class').size())
-#%%
-#%%
-import matplotlib.pyplot as plt
-#%%
-#dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+# col 0-255: data
+# col 256-265: one-hot label
+
+
+#print dataset.shape
+
+X,Y = dataset[range(256)],dataset[range(256,266)]
+#print X.shape
+#print Y.shape
+
+#import matplotlib.pyplot as plt
+# Convert the 1st row to an array to visualize
+#print type(X.iloc[15].values.reshape((16,16),order='C'))
+#plt.imshow(X.iloc[15].values.reshape((16,16),order='C'),cmap='hot')
 #plt.show()
-#%%
-#dataset.hist()
+#plt.imshow(X.iloc[16].values.reshape((16,16)),cmap='gray')
 #plt.show()
-#%%
-#pandas.tools.plotting.scatter_matrix(dataset)
+#plt.imshow(X.iloc[17].values.reshape((16,16)))
+#plt.show()
+#plt.imshow(X.iloc[18].values.reshape((16,16)))
 #plt.show()
 
+#print Y.sum(axis=1)
 
+y=[]
+for r in range(Y.shape[0]):
+	for c in range(256,266):
+		if Y[c].iloc[r]==1:
+			#print r,c-256
+			y.append(c-256)
+import numpy as np
+y=np.array(y)
 
-#%%
+seed=7
 from sklearn import model_selection
-from sklearn import linear_model
-from sklearn import discriminant_analysis
-from sklearn import neighbors
-from sklearn import tree
-from sklearn import naive_bayes
-from sklearn import svm
+X_tr,X_tt,y_tr,y_tt = model_selection.train_test_split(X,y,test_size=.3,random_state=seed)
+
+print X_tr.shape
+print X_tt.shape
+print y_tr.shape
+print y_tt.shape
+
+
 from sklearn import metrics
-#%%
-array = dataset.values
-#%%
-X = array[:,0:4]
-Y = array[:,4]
-#%%
-validation_size = 0.20
+
+def algoTst(model):
+	model.fit(X_tr,y_tr)
+	yhat_tt=model.predict(X_tt)
+	print metrics.accuracy_score(y_tt, yhat_tt)
+	print metrics.confusion_matrix(y_tt, yhat_tt)
+	print metrics.classification_report(y_tt, yhat_tt)
+
+def cvTst(model,FOLD=3):
+	seed=11
+	kfold=model_selection.KFold(n_splits=FOLD,random_state=seed)
+	cv_results = model_selection.cross_val_score(model,X_tr,y_tr,cv=kfold,scoring='accuracy')
+	print cv_results
+
+from sklearn import linear_model
+algoTst(model = linear_model.LogisticRegression())
+cvTst(model = linear_model.LogisticRegression())
 
 
-seed = 7
+from sklearn import svm
+algoTst(model = svm.SVC())
+cvTst(model = svm.SVC())
 
-#%%
-X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
 
-#%%
-scoring = 'accuracy'
-#%%
-models = []
-models.append(('LR', linear_model.LogisticRegression()))
-models.append(('LDA', discriminant_analysis.LinearDiscriminantAnalysis()))
-models.append(('KNN', neighbors.KNeighborsClassifier()))
-models.append(('CART',tree.DecisionTreeClassifier()))
-models.append(('NB', naive_bayes.GaussianNB()))
-models.append(('SVM',svm.SVC()))
-# evaluate each model in turn
-results = []
-names = []
-for name, model in models:
-	kfold = model_selection.KFold(n_splits=10, random_state=seed)
-	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
-	results.append(cv_results)
-	names.append(name)
-	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-	print(msg)
- 
- 
-#%%
-fig = plt.figure()
-fig.suptitle('Algorithm Comparison')
-ax = fig.add_subplot(111)
-plt.boxplot(results)
-ax.set_xticklabels(names)
-plt.show()
+from sklearn import neighbors
+algoTst(model = neighbors.KNeighborsClassifier())
+cvTst(model = neighbors.KNeighborsClassifier())
 
-#%%
-knn = neighbors.KNeighborsClassifier()
-#%%
-knn.fit(X_train, Y_train)
-#%%
-predictions = knn.predict(X_validation)
-#%%
-metrics.accuracy_score(Y_validation, predictions)
-#%%
-metrics.confusion_matrix(Y_validation, predictions)
-#%%
-print(metrics.classification_report(Y_validation, predictions))
+from sklearn import tree
+algoTst(model = tree.DecisionTreeClassifier())
+cvTst(model = tree.DecisionTreeClassifier())
+
+from sklearn import naive_bayes
+algoTst(model = naive_bayes.GaussianNB())
+cvTst(model = naive_bayes.GaussianNB())
+
+from sklearn import discriminant_analysis
+algoTst(model = discriminant_analysis.LinearDiscriminantAnalysis())
+cvTst(model = discriminant_analysis.LinearDiscriminantAnalysis())
+
+
