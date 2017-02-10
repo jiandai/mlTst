9bdc679282255816c41e1c5078c7f02f02845b7d
Jian Dai <daij12@gene.com>
2017-02-09 21:07:27 -0800

    download data to local

--- ml102.py
+++ ml102.py
@@ -12,32 +12,31 @@ import sys
 sys.path.insert(0, os.path.expanduser('~')+'/.local/lib/python2.7/site-packages')
 
 import pandas as pd
-dataset = pd.read_table('https://archive.ics.uci.edu/ml/machine-learning-databases/semeion/semeion.data',sep='\s+',header=None,engine='python')
-
+#dataset = pd.read_table('https://archive.ics.uci.edu/ml/machine-learning-databases/semeion/semeion.data',sep='\s+',header=None,engine='python')
+#dataset = pd.read_table('data/semeion.data.txt',sep='\s+',header=None,engine='python')
+dataset = pd.read_table('data/semeion.data.txt',sep='\s+',header=None)
 # col 0-255: data
 # col 256-265: one-hot label
 
 
-#print dataset.shape
-
-X,Y = dataset[range(256)],dataset[range(256,266)]
+X,Y = dataset[list(range(256))],dataset[list(range(256,266))] # modified on pc by adding list
 #print X.shape
 #print Y.shape
 
 #import matplotlib.pyplot as plt
-# Convert the 1st row to an array to visualize
-#print type(X.iloc[15].values.reshape((16,16),order='C'))
-#plt.imshow(X.iloc[15].values.reshape((16,16),order='C'),cmap='hot')
+#plt.imshow(X.iloc[19].values.reshape((16,16),order='C'),cmap='hot')
 #plt.show()
-#plt.imshow(X.iloc[16].values.reshape((16,16)),cmap='gray')
+#plt.imshow(X.iloc[20].values.reshape((16,16)),cmap='gray')
 #plt.show()
-#plt.imshow(X.iloc[17].values.reshape((16,16)))
+#plt.imshow(X.iloc[21].values.reshape((16,16)))
 #plt.show()
-#plt.imshow(X.iloc[18].values.reshape((16,16)))
+#plt.imshow(X.iloc[22].values.reshape((16,16)))
 #plt.show()
 
 #print Y.sum(axis=1)
 
+
+# convert 1-hot code to a single variable
 y=[]
 for r in range(Y.shape[0]):
 	for c in range(256,266):
@@ -51,10 +50,10 @@ seed=7
 from sklearn import model_selection
 X_tr,X_tt,y_tr,y_tt = model_selection.train_test_split(X,y,test_size=.3,random_state=seed)
 
-print X_tr.shape
-print X_tt.shape
-print y_tr.shape
-print y_tt.shape
+print( X_tr.shape)
+print( X_tt.shape)
+print( y_tr.shape)
+print( y_tt.shape)
 
 
 from sklearn import metrics
@@ -62,15 +61,15 @@ from sklearn import metrics
 def algoTst(model):
 	model.fit(X_tr,y_tr)
 	yhat_tt=model.predict(X_tt)
-	print metrics.accuracy_score(y_tt, yhat_tt)
-	print metrics.confusion_matrix(y_tt, yhat_tt)
-	print metrics.classification_report(y_tt, yhat_tt)
+	print(metrics.accuracy_score(y_tt, yhat_tt))
+	print(metrics.confusion_matrix(y_tt, yhat_tt))
+	print(metrics.classification_report(y_tt, yhat_tt))
 
 def cvTst(model,FOLD=3):
 	seed=11
 	kfold=model_selection.KFold(n_splits=FOLD,random_state=seed)
 	cv_results = model_selection.cross_val_score(model,X_tr,y_tr,cv=kfold,scoring='accuracy')
-	print cv_results
+	print(cv_results)
 
 from sklearn import linear_model
 algoTst(model = linear_model.LogisticRegression())
