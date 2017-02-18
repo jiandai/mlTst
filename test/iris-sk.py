b351859e86dba7cf409fce7ca601fa3c19c49c0d
Jian Dai <daij12@gene.com>
2017-02-18 07:03:30 -0800

    iris test

--- test/iris-sk.py
+++ test/iris-sk.py
@@ -3,9 +3,9 @@
 version <20170209 by Jian: load iris from sklearn
 version 20170209 by Jian: play /w algo
 ref: http://machinelearningmastery.com/a-gentle-introduction-to-scikit-learn-a-python-machine-learning-library/
+version 20170216 by Jian: more on sklearn
+
 '''
-
-
 
 from sklearn import datasets
 dataset = datasets.load_iris()
@@ -13,8 +13,9 @@ dataset = datasets.load_iris()
 # fit a CART model to the data
 from sklearn.tree import DecisionTreeClassifier
 model = DecisionTreeClassifier()
-model.fit(dataset.data, dataset.target)
 print(model)
+
+model.fit(dataset.data, dataset.target)
 # make predictions
 expected = dataset.target
 predicted = model.predict(dataset.data)
@@ -29,15 +30,11 @@ print(metrics.confusion_matrix(expected, predicted))
 iris = dataset
 print(iris.keys())
 
-
-
 # create a Iris data frame
 import pandas as pd
 irisDF=pd.concat([pd.DataFrame(iris.data,columns=iris.feature_names),pd.DataFrame(iris.target,columns=['SpeciesCode'])],axis=1) 
 
-
 irisDF['Species']=iris.target_names [irisDF.SpeciesCode]
-
 
 print(irisDF.head())
 
